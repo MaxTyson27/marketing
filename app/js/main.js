54900c316d8dcfd93f5a60ad37bae85e928a7774
MaxTyson27 <72412722+MaxTyson27@users.noreply.github.com>
2021-11-12 00:38:12 +0300

    update

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,12 @@
 $(function () {
 
+  var mixer = mixitup('.portfolio__inner');
+
+
+  $('.slider__inner').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-next.svg" alt=""></button>',
+  });
 
 });
